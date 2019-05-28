4548ea558bc46adecb8ac3972fe321278e140ed2
sujanie <36542245+sujanie@users.noreply.github.com>
2019-05-28 10:25:43 +0530

    Update proceed1.php

--- proceed1.php
+++ proceed1.php
@@ -6,7 +6,7 @@
 	$num=$_POST['number1'];
 	//$num1=$_POST['number2'];
 	
-	$url= "http://0.0.0.0:8080/?city=".$city."&checkIn=".$checkIn."&checkOut=".$checkOut."&num=1&num1=".$num;
+	$url= "https://pythondata.herokuapp.com:8080/?city=".$city."&checkIn=".$checkIn."&checkOut=".$checkOut."&num=1&num1=".$num;
 	//$url="http://127.0.0.1:8080/?city=Singapore&checkIn=05/05/2019&checkOut=05/06/2019&num=1&num1=2";
 
 	$xml = file_get_contents($url);
