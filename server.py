2719ccc13be78dbf87af7f06a51beaee0f15cfeb
sujanie <36542245+sujanie@users.noreply.github.com>
2019-05-29 10:14:24 +0530

    Update server.py

--- server.py
+++ server.py
@@ -42,7 +42,7 @@ def run():
         try:
                 #Create a web server and define the handler to manage the
                 #incoming request
-                server = HTTPServer(('0.0.0.0', PORT_NUMBER), myHandler)
+                server = HTTPServer(('127.0.0.1', PORT_NUMBER), myHandler)
                 print ("Started httpserver on port " , PORT_NUMBER)
                 #Wait forever for incoming htto requests
                 server.serve_forever()
